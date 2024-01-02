a684c2ae9ef3cfddc061a1b4dbef2f6c7c064ffe
Barbara Mascena <barbara.mascena@gmail.com>
2024-01-02 19:30:38 -0300

    meu primeiro commit

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,5 +1,9 @@
 const analyzer = {  
   getWordCount: (text) => {
+        const words = text.trim().split(/\s+/).filter(Boolean)
+        return words.length;
+      },
+    
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import analyzer from './analyzer.js';
-
+const topicosDaLista = (Text) => {
+    const wordcount = analyzer.getWordCount(document.querySelector('[name="user-input"]').value)
+  
+    document.querySelector('[data-testid = "word-count"]').textContent = `Contagem de palavras: ${wordcount}`;
+  
+  }
+  document.querySelector('[name="user-input"]').addEventListener("input",topicosDaLista)
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
